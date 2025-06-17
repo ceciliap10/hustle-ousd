0c10beca30a0512d363676b6e3685e03ed6434e2
Cecilia Palau <ceciliapalau@Cecilias-MacBook-Pro.local>
2025-06-17 12:17:25 -0700

    lab2

---
+++ lab2.py
@@ -0,0 +1,13 @@
+name = "Cecilia Palau"
+
+nickname = "Ceci"
+
+age = 14
+
+usedbefore = False
+
+hobbies = ["soccer", "surfing", "watchng tv shows"]
+
+my_faves = {'Movie' : 'Totoro'}
+
+print(age)
