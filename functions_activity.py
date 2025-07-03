b49fb9521ab04bd22cebe22a6649d8b8b515ea5e
Cecilia Palau <ceciliapalau@Cecilias-MacBook-Pro.local>
2025-07-03 13:49:00 -0700

    this is my updated version

--- functions_activity.py
+++ functions_activity.py
@@ -1,8 +1,8 @@
 menu = {'Pizza' : 2.99, 'Burger' : 3.99, 'Hot Dog' : 1.99, 'Cheese' : 0.59, 'Ice Cream' : 1.49, 'Churro' : 0.79, 'Soda' : 0.89}
 
-def total_price = (food2 + food4):
+def total_price(food2 + food4):
 
-food2 = menu [food2]
+    food2 = menu [food2]
 
 food4 = menu [food4]
 
@@ -10,27 +10,27 @@ print(total_price('Hot Dog', 'Soda'))
 
 def price_difference(food2, food4):
 
-price2 = menu [food1]
+    price2 = menu [food1]
 
-price4 = menu [food4]
+    price4 = menu [food4]
 
-return %
+    return difference
 
-print(price % (Hot Dog, Soda))
+    print(price difference{Hot Dog, Soda})
 
 def Inflation (food, multiplier):
 
-menu[food] = menu [food] * multiplier
+    menu[food] = menu [food] * multiplier
 
-retrun menu
+    retrun;menu
 
 def deflation(item, divide):
 
-menu [food] = menu[food] / divide
+    menu [food] = menu[food] / divide
 
-return menu
+    return menu
 
-def add ('Tomato': 0.50)
+def add{'Tomato': 0.50}
     
 print(add)
 
