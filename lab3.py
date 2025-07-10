35737dce6014277d44303783dcd933978ffbfe32
Cecilia Palau <ceciliapalau@Cecilias-MacBook-Pro.local>
2025-07-10 11:13:27 -0700

    This is my updated lab3

--- lab3.py
+++ lab3.py
@@ -5,7 +5,7 @@
 food = "Pupusas de Frijol"
 
 print(food[0:3])
-print (food[-4:])
+print (food[-3:])
 
 first_last= food[0] + food[-1]
 
@@ -17,6 +17,8 @@ print(food_list)
 
 joined_food= ''.join(food_list)
 
+#Lists
+
 number_list= [2,10,20,100,15]
 
 number_list.append(4)
@@ -25,20 +27,20 @@ print(number_list)
 
 number_list.insert(3,"element")
 
-number_list.get("Elisabetta Dami")
-
-number_list.pop("The Very Hungry Catepillar")
+number_list.pop(1)
 
 number_list.remove(number_list[1])
 
 print (number_list [0:3])
-print (number_list [-4:])
+print (number_list [-3:])
+
+#Dictionaries
 
 books = {"Elisabetta Dami": "Four Mice Deep in the Jungle","Dr.Seuss": "Cat In The Hat", "Erik Carle":"The Very Hungry Catepillar", "Marcus Pfister": "The Rainbow Fish"}
 
 
 print(books.keys ())
 print(books.values ())
-print(books.get())
-print(books.pop())
-del books ["Dr.Seuss"]
+print(books.get("Erik Carle"))
+print(books.pop("Dr.Seuss"))
+del books ["Elisabetta Dami"]
