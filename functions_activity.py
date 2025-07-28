ae988efce52a3a5b58cf4e962438cbffd61a1019
Cecilia Palau <ceciliapalau@Cecilias-MacBook-Pro.local>
2025-07-28 11:50:02 -0700

    This is my updated functions and operators activity.

--- functions_activity.py
+++ functions_activity.py
@@ -1,37 +1,43 @@
 menu = {'Pizza' : 2.99, 'Burger' : 3.99, 'Hot Dog' : 1.99, 'Cheese' : 0.59, 'Ice Cream' : 1.49, 'Churro' : 0.79, 'Soda' : 0.89}
 
-def total_price(food2 + food4):
+def total_price (food2, food4):
 
-    food2 = menu [food2]
+    food2 = menu [1]
 
-food4 = menu [food4]
+    food4 = menu [3]
 
-print(total_price('Hot Dog', 'Soda'))
+print(total_price,{'HotDog', 'Soda'})
 
 def price_difference(food2, food4):
 
-    price2 = menu [food1]
+    price2 = menu [food2]
 
     price4 = menu [food4]
 
-    return difference
+    return price_difference
 
-    print(price difference{Hot Dog, Soda})
+    print(price_difference,{'Hot Dog', 'Soda'})
 
 def Inflation (food, multiplier):
 
     menu[food] = menu [food] * multiplier
 
-    retrun;menu
+    retrun:menu
 
 def deflation(item, divide):
 
-    menu [food] = menu[food] / divide
+    menu [food2] = menu[food4] / divide
 
     return menu
 
-def add{'Tomato': 0.50}
-    
-print(add)
 
+def discount(food2,amount):
+    if amount >=  5:
+        food_discount = menu[food2] / 2
+        return food_discount
+
+    else:
+        return menu[food2]
+        
+print(discount("Soda", 7))
 
